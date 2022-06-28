b244a7d607c65f102226de0d2baf0c5bd88806ec
Santiago Diaz Monvale <santiago.diaz.monvale@gmail.com>
2022-06-27 23:01:47 -0300

    Creando repositorio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* Variables */
+let nombre;
+let apellido;
+let email;
+let edad;
+let numero1;
+let numero2;
+let numero3;
+let numero4;
+
+
+/* Alerta datos de Usuario*/
+
+nombre = prompt("Introduci tu nombre");
+apellido = prompt("Introduci tu apellido");
+
+alert("Bienvenido"+" "+nombre+" "+apellido);
+
+/* Alerta suma de 2 numeros */
+
+numero1 = parseInt(prompt("Introducir el primer numero que quiera sumar"));
+numero2 = parseInt(prompt("Introducir el segundo numero que quiera sumar"));
+
+let resultado = numero1 + numero2;
+
+alert(`El resultado de la suma ${numero1} mas ${numero2} es ${resultado}`);
+
+
