4ee8b4bba9f3801d809720ac7ec07e385889f185
Santiago Diaz Monvale <santiago.diaz.monvale@gmail.com>
2022-07-11 21:54:20 -0300

    Agregando estructura base HTML y haciendo objetos para las provincias

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 /* Variables */
 let nombre;
 let apellido;
-let email;
-let edad;
-let numero1;
-let numero2;
-let numero3;
-let numero4;
+
 /* Alerta datos de Usuario*/
 
-nombre = prompt("Introduci tu nombre");
+/* nombre = prompt("Introduci tu nombre");
 apellido = prompt("Introduci tu apellido");
 
-alert("Bienvenido" + " " + nombre + " " + apellido);
+alert("Bienvenido" + " " + nombre + " " + apellido); */
 
-/* Alerta suma de 2 numeros */
+/* Datos sobre provincias */
 
-numero1 = parseInt(prompt("Introducir el primer numero que quiera sumar"));
-numero2 = parseInt(prompt("Introducir el segundo numero que quiera sumar"));
+function provincia (nombre,poblacion,territorio){
+    this.nombre = nombre;
+    this.poblacion = poblacion;
+    this.territorio = territorio;
+    this.datos = function(){ console.log("La provincia de "+this.nombre+" tiene una poblacion de "+this.poblacion+" habitantes y una superficie de "+this.territorio+" Km²")};
 
-let resultado = numero1 + numero2;
+}
+const buenos_aires = new provincia ("Buenos Aires" , "15625084" , "307571")
 
-alert(`El resultado de la suma ${numero1} mas ${numero2} es ${resultado}`);
+const entre_rios = new provincia ("Entre rios" , "1405890" , "78781")
+
+const cordoba = new provincia ("Cordoba"," 3308876","165321")
+
+buenos_aires.datos();
+entre_rios.datos();
+cordoba.datos();
+
+let resultado = parseInt(buenos_aires.poblacion) + parseInt(cordoba.poblacion);
+
+console.log("Entre Buenos Aires y Cordoba hay "+resultado+" habitantes")
+
+
+
 
 /* Alerta con bucle */
 
 for (let i = 0; i < 3; i++){
-    alert("Este es un mensaje que se repetira 3 veces");
+    console.log("Este es un mensaje que se repetira 3 veces");
 
 }
